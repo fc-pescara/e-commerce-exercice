4deb3feca0c14c898e93f1ccdd79f59fa9cadff7
fc-pescara <88330165+fc-pescara@users.noreply.github.com>
2022-03-06 18:18:01 +0100

    Add files via upload

---
+++ E COMMERCE KEVIN/java.js
@@ -0,0 +1,19 @@
+fetch('https://fakestoreapi.com/products') 
+            .then(res=>res.json()) 
+            .then(json=>console.log(json)) 
+           
+            
+
+                
+          
+function Products (image,titre){
+    this.image = image ,
+    this.titre = titre 
+}
+
+const product1 = new Products 
+
+
+
+
+
